5c4fbe4d961798d5c8a9f422424c6c6992c147c4
Linh Vu <vuvlinh.23@gmail.com>
2020-08-07 16:59:47 +0700

    added solution not using string for palindromeNumber

--- palindromeNumber/palindromeNumber.py
+++ palindromeNumber/palindromeNumber.py
@@ -21,15 +21,34 @@ Coud you solve it without converting the integer to a string?
 
 '''
 
+import math
+
 class Solution:
     # Turn to string and compare with old one
     def isPalindromeUsingString(self, x: int) -> bool:
       if x < 0: return False
       return str(x) == str(x)[::-1]
 
+    def isPalindromeNotUsingString(self, x: int) -> bool:
+      if x < 0 or (x % 10 == 0 and x != 0):
+        return False
+      
+      rev = 0
+      while x > rev:
+        rev = rev * 10 + x % 10
+        x = math.floor(x / 10)
+      
+      # When the length is an odd number, we can get rid of the middle digit by revertedNumber/10
+      # For example when the input is 12321, at the end of the while loop we get x = 12, revertedNumber = 123,
+      # since the middle digit doesn't matter in palidrome(it will always equal to itself), we can simply get rid of it.
+      return x == rev or x == math.floor(rev/10)
 
-input = 123
+
+input = 1231321
 
 s = Solution()
 
-result = s.isPalindromeUsingString(input)
+# result = s.isPalindromeUsingString(input)
+result = s.isPalindromeNotUsingString(input)
+
+print(result)
