47dd5d2c224ebd38f820378f3d48650f3ab8177f
Linh Vu <vuvlinh.23@gmail.com>
2020-09-11 15:45:35 +0700

    added solution for minStack

---
+++ minStack/sol.py
@@ -0,0 +1,71 @@
+'''
+
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+getMin() -- Retrieve the minimum element in the stack.
+
+
+Example 1:
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+
+
+Constraints:
+
+Methods pop, top and getMin operations will always be called on non-empty stacks.
+
+'''
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.minStack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        if len(self.minStack) == 0 or self.minStack[-1] >= x:
+          self.minStack.append(x)
+
+    def pop(self) -> None:
+      if len(self.stack) > 0:
+        lastItem = self.stack.pop()
+        if lastItem == self.minStack[-1]:
+          self.minStack.pop()
+
+    def top(self) -> int:
+      if len(self.stack) > 0:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+      if len(self.stack) > 0:
+        return self.minStack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
